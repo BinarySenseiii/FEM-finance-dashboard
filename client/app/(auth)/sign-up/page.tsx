cde52e6c7e81c005780508843dc59548684f77d1
Faisal Tariq <faisaltariq183@gmail.com>
2024-09-22 23:42:14 +0400

    feat: add base authentication layout structure

---
+++ client/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const SignUpPage = () => {
+  return (
+    <form>
+      <h1 className='text-preset-1 text-grey-900'>Sign Up</h1>
+    </form>
+  )
+}
+
+export default SignUpPage
--- client/components/design-system/design-showcase.tsx
+++
@@ -1,5 +0,0 @@
-const DesignShowcase = () => {
-  return <div>DesignShowcase</div>
-}
-
-export default DesignShowcase
